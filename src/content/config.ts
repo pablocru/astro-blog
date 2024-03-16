7ce0d0748acec82cd5b6e5a4bec9f666d3315dda
pabcrudel <pabcrudel@gmail.com>
2024-03-16 19:30:20 +0100

    feat: Add type declaration for posts

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+// Source: https://docs.astro.build/en/tutorials/add-content-collections/#create-a-collection-for-your-blog-posts
+
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    postTile: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }),
+    tags: z.array(z.string())
+  })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+};
