ccb13c4c7b8908060a89f839882a95c431b4f00e
pabcrudel <pabcrudel@gmail.com>
2024-03-16 19:32:37 +0100

    feat: Add an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+const REPO_NAME = "/astro-blog/";
+
 // https://astro.build/config
 export default defineConfig({
-  base: import.meta.env.PROD ? '/astro-blog/' : undefined
+  site: "https://pabcrudel.github.io" + REPO_NAME,
+  base: import.meta.env.PROD ? REPO_NAME : undefined
 });
