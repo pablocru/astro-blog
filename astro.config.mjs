1ff963562ee66bc3bae47f5de175475d0c865f08
pablocru <pablocru@outlook.com>
2024-03-29 11:56:26 +0100

    feat: Update links to the new account name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ const REPO_NAME = "/astro-blog/";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pabcrudel.github.io" + REPO_NAME,
+  site: "https://pablocru.github.io" + REPO_NAME,
   base: import.meta.env.PROD ? REPO_NAME : undefined
 });
