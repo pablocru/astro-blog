145f46e6a9281cb0517cb882af53c86ee46851d8
pablocru <pablocru@outlook.com>
2024-03-29 11:54:51 +0100

    feat: Remove Sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
 
 const REPO_NAME = "/astro-blog/";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pabcrudel.github.io" + REPO_NAME,
-  base: import.meta.env.PROD ? REPO_NAME : undefined,
-  integrations: [sitemap()]
+  base: import.meta.env.PROD ? REPO_NAME : undefined
 });
