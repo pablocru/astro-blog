bc1071a81927cb0593fba770a65a5d4d5e3bc93a
pablocru <pablocru@outlook.com>
2024-04-29 21:15:57 +0200

    refactor: Configure site and base properties in Astro config file properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-const REPO_NAME = "/astro-blog/";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pablocru.github.io" + REPO_NAME,
-  base: import.meta.env.PROD ? REPO_NAME : undefined
+  site: "https://pablocru.github.io",
+  base: "astro-blog"
 });
